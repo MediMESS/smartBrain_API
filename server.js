887b85b3c86c98d9dfa4a66ecd2905f58a18e92f
Mehdi Messarat <medimessarat@gmail.com>
2020-12-28 17:06:20 +0100

    fixing the cors

--- server.js
+++ server.js
@@ -27,6 +27,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const corsConfig = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type,Accept,Authorization,adminType,userType"
+  );
+  next();
+};
+app.use(corsConfig);
+
 app.get("/", (req, res) => {
   res.send(db.users);
 });
