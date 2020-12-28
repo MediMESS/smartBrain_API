601be85036e073797f0968a5b24ad88cb8c136d3
Mehdi Messarat <medimessarat@gmail.com>
2020-12-28 16:46:36 +0100

    refactor project and make it functional

--- controllers/register.js
+++ controllers/register.js
@@ -1,26 +1,32 @@
 const handleRegister = (req, res, db, bcrypt) => {
-
-    const {name, email, password} = req.body;
-    bcrypt.hash(password, 10, function(err, hash) {
-      const hashedPassword = hash;
-      db('login').insert({
-        email: email,
-        hash: hashedPassword
-      });
-      db('users')
-      .returning('*')
-      .insert({
-        name: name,
-        email: email,
-        joined: new Date()
-      })
-      .then(user =>{
-        res.json(user[0])
-      })
-      .catch(err => {res.status(400).json("Error, Unable to register, account already exist")});
-
-    });
-
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
